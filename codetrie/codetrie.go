12119f3d580fbf957cfa1b3e8607062e1cfb491f
Sina Mahmoodi <itz.s1na@gmail.com>
2020-10-27 15:16:40 +0100

    codetrie: implement chunkify logic

--- codetrie/codetrie.go
+++ codetrie/codetrie.go
@@ -2,6 +2,8 @@ package codetrie
 
 import (
 	"math"
+
+	"github.com/ethereum/go-ethereum/core/vm"
 )
 
 type Chunk struct {
@@ -16,5 +18,46 @@ func NewChunk() *Chunk {
 func Chunkify(code []byte, chunkSize uint) []*Chunk {
 	numChunks := uint(math.Ceil(float64(len(code)) / float64(chunkSize)))
 	chunks := make([]*Chunk, numChunks)
+
+	for i := uint(0); i < numChunks; i++ {
+		startIdx := i * chunkSize
+		endIdx := (i + 1) * chunkSize
+		if i == numChunks-1 {
+			endIdx = uint(len(code))
+		}
+		chunks[i] = &Chunk{fio: 0, code: code[startIdx:endIdx]}
+	}
+
+	setFIO(chunks)
+
 	return chunks
+}
+
+func setFIO(chunks []*Chunk) {
+	if len(chunks) < 2 {
+		return
+	}
+
+	chunkSize := len(chunks[0].code)
+
+	for i, chunk := range chunks {
+		if i == len(chunks)-1 {
+			break
+		}
+
+		for j, op := range chunk.code {
+			opcode := vm.OpCode(op)
+			if opcode.IsPush() {
+				size := getPushSize(opcode)
+				if j+size >= chunkSize {
+					nextFIO := (j + size + 1) - chunkSize
+					chunks[i+1].fio = uint8(nextFIO)
+				}
+			}
+		}
+	}
+}
+
+func getPushSize(opcode vm.OpCode) int {
+	return (int(opcode) - 0x60) + 1
 }
--- codetrie/codetrie_test.go
+++ codetrie/codetrie_test.go
@@ -1,33 +1,94 @@
 package codetrie
 
 import (
+	"bytes"
 	"encoding/hex"
+	"fmt"
 	"strings"
 	"testing"
 )
 
+type TChunk struct {
+	fio  uint8
+	code string
+}
+
 type ChunkifyTest struct {
-	Input       string
-	ExpectedNum int
+	Input  string
+	Chunks []TChunk
 }
 
 func TestChunkifyNum(t *testing.T) {
 	testCases := []ChunkifyTest{
 		{
-			Input:       "6000",
-			ExpectedNum: 1,
+			Input: "6000",
+			Chunks: []TChunk{
+				{
+					fio:  0,
+					code: "6000",
+				},
+			},
 		},
 		{
-			Input:       strings.Repeat("6000", 15) + "00", // Len: 31
-			ExpectedNum: 1,
+			Input: strings.Repeat("6000", 15) + "00", // Len: 31
+			Chunks: []TChunk{
+				{
+					fio:  0,
+					code: strings.Repeat("6000", 15) + "00",
+				},
+			},
 		},
 		{
-			Input:       strings.Repeat("6000", 16), // Len: 32
-			ExpectedNum: 1,
+			Input: strings.Repeat("6000", 16), // Len: 32
+			Chunks: []TChunk{
+				{
+					fio:  0,
+					code: strings.Repeat("6000", 16),
+				},
+			},
 		},
 		{
-			Input:       strings.Repeat("6000", 17), // Len: 32
-			ExpectedNum: 2,
+			Input: strings.Repeat("6000", 17), // Len: 32
+			Chunks: []TChunk{
+				{
+					fio:  0,
+					code: strings.Repeat("6000", 16),
+				},
+				{
+					fio:  0,
+					code: "6000",
+				},
+			},
+		},
+		{
+			Input: strings.Repeat("58", 31) + "605b" + strings.Repeat("58", 30),
+			Chunks: []TChunk{
+				{
+					fio:  0,
+					code: strings.Repeat("58", 31) + "60",
+				},
+				{
+					fio:  1,
+					code: "5b" + strings.Repeat("58", 30),
+				},
+			},
+		},
+		{
+			Input: strings.Repeat("58", 31) + "7f" + strings.Repeat("5b", 32) + strings.Repeat("58", 30),
+			Chunks: []TChunk{
+				{
+					fio:  0,
+					code: strings.Repeat("58", 31) + "7f",
+				},
+				{
+					fio:  32,
+					code: strings.Repeat("5b", 32),
+				},
+				{
+					fio:  0,
+					code: strings.Repeat("58", 30),
+				},
+			},
 		},
 	}
 
@@ -37,8 +98,22 @@ func TestChunkifyNum(t *testing.T) {
 			t.Error(err)
 		}
 		chunks := Chunkify(code, 32)
-		if len(chunks) != c.ExpectedNum {
-			t.Errorf("%v: invalid number of chunks: expected %d, got %d", t.Name(), c.ExpectedNum, len(chunks))
+		if len(chunks) != len(c.Chunks) {
+			t.Errorf("%v: invalid number of chunks: expected %d, got %d", t.Name(), len(c.Chunks), len(chunks))
+		}
+		for i, chunk := range chunks {
+			expectedChunk := c.Chunks[i]
+			fmt.Println(i, chunk)
+			if chunk.fio != expectedChunk.fio {
+				t.Errorf("%v: invalid chunk FIO: expected %d, got %d", t.Name(), expectedChunk.fio, chunk.fio)
+			}
+			expectedCode, err := hex.DecodeString(expectedChunk.code)
+			if err != nil {
+				t.Error(err)
+			}
+			if !bytes.Equal(chunk.code, expectedCode) {
+				t.Errorf("%v: invalid chunk code: expected %s, got %s", t.Name(), expectedChunk.code, hex.EncodeToString(chunk.code))
+			}
 		}
 	}
 }

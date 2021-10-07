b0b2f64758b47254cd85366f7c00b0ae119179b8
Jared Wasinger <j-wasinger@hotmail.com>
2021-10-07 10:27:35 +0200

    core/vm: separate vm.StateDB interface into StateReader and StateWriter

--- core/vm/interface.go
+++ core/vm/interface.go
@@ -23,31 +23,15 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
-// StateDB is an EVM database for full state querying.
-type StateDB interface {
-	CreateAccount(common.Address)
-
-	SubBalance(common.Address, *big.Int)
-	AddBalance(common.Address, *big.Int)
+type StateReader interface {
 	GetBalance(common.Address) *big.Int
-
 	GetNonce(common.Address) uint64
-	SetNonce(common.Address, uint64)
-
 	GetCodeHash(common.Address) common.Hash
 	GetCode(common.Address) []byte
-	SetCode(common.Address, []byte)
 	GetCodeSize(common.Address) int
-
-	AddRefund(uint64)
-	SubRefund(uint64)
 	GetRefund() uint64
-
 	GetCommittedState(common.Address, common.Hash) common.Hash
 	GetState(common.Address, common.Hash) common.Hash
-	SetState(common.Address, common.Hash, common.Hash)
-
-	Suicide(common.Address) bool
 	HasSuicided(common.Address) bool
 
 	// Exist reports whether the given account exists in state.
@@ -57,7 +41,6 @@ type StateDB interface {
 	// is defined according to EIP161 (balance = nonce = code = 0).
 	Empty(common.Address) bool
 
-	PrepareAccessList(sender common.Address, dest *common.Address, precompiles []common.Address, txAccesses types.AccessList)
 	AddressInAccessList(addr common.Address) bool
 	SlotInAccessList(addr common.Address, slot common.Hash) (addressOk bool, slotOk bool)
 	// AddAddressToAccessList adds the given address to the access list. This operation is safe to perform
@@ -66,14 +49,43 @@ type StateDB interface {
 	// AddSlotToAccessList adds the given (address,slot) to the access list. This operation is safe to perform
 	// even if the feature/fork is not active yet
 	AddSlotToAccessList(addr common.Address, slot common.Hash)
+	ForEachStorage(common.Address, func(common.Hash, common.Hash) bool) error
+}
+
+type StateWriter interface {
+	CreateAccount(common.Address)
+
+	SubBalance(common.Address, *big.Int)
+	AddBalance(common.Address, *big.Int)
+
+	SetNonce(common.Address, uint64)
+
+
+	SetCode(common.Address, []byte)
+
+	AddRefund(uint64)
+	SubRefund(uint64)
+
+
+	SetState(common.Address, common.Hash, common.Hash)
+
+	Suicide(common.Address) bool
+
+
+	PrepareAccessList(sender common.Address, dest *common.Address, precompiles []common.Address, txAccesses types.AccessList)
+
 
 	RevertToSnapshot(int)
 	Snapshot() int
 
 	AddLog(*types.Log)
 	AddPreimage(common.Hash, []byte)
+}
 
-	ForEachStorage(common.Address, func(common.Hash, common.Hash) bool) error
+// StateDB is an EVM database for full state querying.
+type StateDB interface {
+    StateReader
+    StateWriter
 }
 
 // CallContext provides a basic interface for the EVM calling conventions. The EVM

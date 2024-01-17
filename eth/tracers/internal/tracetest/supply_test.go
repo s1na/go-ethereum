d6d45e5eae2c50ec581141dc83b7c94cab0eeb00
Chris Ziogas <ziogaschr@gmail.com>
2024-01-17 15:43:02 +0200

    Refactor supply live tracer tests

--- eth/tracers/internal/tracetest/supply_test.go
+++ eth/tracers/internal/tracetest/supply_test.go
@@ -19,6 +19,7 @@ package tracetest
 import (
 	"bufio"
 	"encoding/json"
+	"fmt"
 	"math/big"
 	"os"
 	"reflect"
@@ -41,25 +42,94 @@ import (
 	_ "github.com/ethereum/go-ethereum/eth/tracers/live"
 )
 
-func TestSupplyTracer(t *testing.T) {
-	var (
-		merger = consensus.NewMerger(rawdb.NewMemoryDatabase())
-		engine = beacon.New(ethash.NewFaker())
 
-		aa = common.HexToAddress("0x000000000000000000000000000000000000aaaa")
-		// A sender who makes transactions, has some funds
+func emptyBlockGenerationFunc(b *core.BlockGen) {}
+
+func TestSupplyGenesisAlloc(t *testing.T) {
+	var (
 		key1, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
 		key2, _ = crypto.HexToECDSA("8a1f9a8f95be41cd7ccb6168179afb4504aefe388d1e14474d32c45c72ce7b7a")
 		addr1   = crypto.PubkeyToAddress(key1.PublicKey)
 		addr2   = crypto.PubkeyToAddress(key2.PublicKey)
 		funds   = new(big.Int).Mul(common.Big1, big.NewInt(params.Ether))
-		config  = *params.AllEthashProtocolChanges
+
+		config = *params.AllEthashProtocolChanges
 
 		gspec = &core.Genesis{
 			Config: &config,
 			Alloc: core.GenesisAlloc{
 				addr1: {Balance: funds},
 				addr2: {Balance: funds},
+			},
+		}
+	)
+
+	expected := live.SupplyInfo{
+		Delta:       new(big.Int).Mul(common.Big2, big.NewInt(params.Ether)),
+		Reward:      common.Big0,
+		Withdrawals: common.Big0,
+		Burn:        common.Big0,
+		Number:      0,
+		Hash:        common.HexToHash("0xbcc9466e9fc6a8b56f4b29ca353a421ff8b51a0c1a58ca4743b427605b08f2ca"), ParentHash: common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000000"),
+	}
+
+	out, err := testSupplyTracer(gspec, emptyBlockGenerationFunc)
+	if err != nil {
+		t.Fatalf("failed to test supply tracer: %v", err)
+	}
+
+	actual := out[expected.Number]
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("incorrect supply info: expected %+v, got %+v", expected, actual)
+	}
+}
+
+func TestSupplyRewards(t *testing.T) {
+	var (
+		config = *params.AllEthashProtocolChanges
+
+		gspec = &core.Genesis{
+			Config: &config,
+		}
+	)
+
+	expected := live.SupplyInfo{
+		Delta:       new(big.Int).Mul(common.Big2, big.NewInt(params.Ether)),
+		Reward:      new(big.Int).Mul(common.Big2, big.NewInt(params.Ether)),
+		Withdrawals: common.Big0,
+		Burn:        common.Big0,
+		Number:      1,
+		Hash:        common.HexToHash("0xcbb08370505be503dafedc4e96d139ea27aba3cbc580148568b8a307b3f51052"), ParentHash: common.HexToHash("0xadeda0a83e337b6c073e3f0e9a17531a04009b397a9588c093b628f21b8bc5a3"),
+	}
+
+	out, err := testSupplyTracer(gspec, emptyBlockGenerationFunc)
+	if err != nil {
+		t.Fatalf("failed to test supply tracer: %v", err)
+	}
+
+	actual := out[expected.Number]
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("incorrect supply info: expected %+v, got %+v", expected, actual)
+	}
+}
+
+func TestSupplyEip1559Burn(t *testing.T) {
+	var (
+		config = *params.AllEthashProtocolChanges
+
+		aa = common.HexToAddress("0x000000000000000000000000000000000000aaaa")
+		// A sender who makes transactions, has some funds
+		key1, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+		addr1   = crypto.PubkeyToAddress(key1.PublicKey)
+		funds   = new(big.Int).Mul(common.Big1, big.NewInt(params.Ether))
+
+		gspec = &core.Genesis{
+			Config:  &config,
+			BaseFee: big.NewInt(params.InitialBaseFee),
+			Alloc: core.GenesisAlloc{
+				addr1: {Balance: funds},
 				// The address 0xAAAA sloads 0x00 and 0x01
 				aa: {
 					Code: []byte{
@@ -75,186 +145,150 @@ func TestSupplyTracer(t *testing.T) {
 		}
 	)
 
-	gspec.Config.BerlinBlock = big.NewInt(1)
-	gspec.Config.LondonBlock = big.NewInt(2)
-	gspec.Config.ArrowGlacierBlock = big.NewInt(2)
-	gspec.Config.GrayGlacierBlock = big.NewInt(2)
-
-	blockTime := uint64(10)
-	shanghaiBlockNumber := uint64(3)
-	shanghaiTime := gspec.Timestamp + (blockTime * shanghaiBlockNumber) + 1
-	gspec.Config.ShanghaiTime = &shanghaiTime
-
 	signer := types.LatestSigner(gspec.Config)
 
-	tests := []struct {
-		name     string
-		input    func(b *core.BlockGen)
-		expected live.SupplyInfo
-	}{
-		{"Genesis",
-			func(b *core.BlockGen) {},
-			live.SupplyInfo{
-				Delta:       new(big.Int).Mul(common.Big2, big.NewInt(params.Ether)),
-				Reward:      common.Big0,
-				Withdrawals: common.Big0,
-				Burn:        common.Big0,
-				Number:      0,
-				Hash:        common.HexToHash("0xa20143bad7de540bd4747be05f9a17d2708aff19c8db72fa9026cc11bc151727"), ParentHash: common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000000"),
-			},
-		},
-		{"Rewards",
-			func(b *core.BlockGen) {},
-			live.SupplyInfo{
-				Delta:       new(big.Int).Mul(common.Big2, big.NewInt(params.Ether)),
-				Reward:      new(big.Int).Mul(common.Big2, big.NewInt(params.Ether)),
-				Withdrawals: common.Big0,
-				Burn:        common.Big0,
-				Number:      1,
-				Hash:        common.HexToHash("0x3053472d528da389264ca290e61d330fec1138ccb9d9ef686d5f1a8b68069b3b"), ParentHash: common.HexToHash("0xa20143bad7de540bd4747be05f9a17d2708aff19c8db72fa9026cc11bc151727"),
-			},
-		},
-		{"London",
-			func(b *core.BlockGen) {
-				// One transaction to 0xAAAA
-				accesses := types.AccessList{types.AccessTuple{
-					Address:     aa,
-					StorageKeys: []common.Hash{{0}},
-				}}
+	eip1559BlockGenerationFunc := func(b *core.BlockGen) {
+		// One transaction to 0xAAAA
+		accesses := types.AccessList{types.AccessTuple{
+			Address:     aa,
+			StorageKeys: []common.Hash{{0}},
+		}}
 
-				txdata := &types.DynamicFeeTx{
-					ChainID:    gspec.Config.ChainID,
-					Nonce:      0,
-					To:         &aa,
-					Gas:        30000,
-					GasFeeCap:  new(big.Int).Mul(big.NewInt(5), big.NewInt(params.GWei)),
-					GasTipCap:  big.NewInt(2),
-					AccessList: accesses,
-					Data:       []byte{},
-				}
-				tx := types.NewTx(txdata)
-				tx, _ = types.SignTx(tx, signer, key1)
+		txdata := &types.DynamicFeeTx{
+			ChainID:    gspec.Config.ChainID,
+			Nonce:      0,
+			To:         &aa,
+			Gas:        30000,
+			GasFeeCap:  new(big.Int).Mul(big.NewInt(5), big.NewInt(params.GWei)),
+			GasTipCap:  big.NewInt(2),
+			AccessList: accesses,
+			Data:       []byte{},
+		}
+		tx := types.NewTx(txdata)
+		tx, _ = types.SignTx(tx, signer, key1)
 
-				b.AddTx(tx)
-			},
-			live.SupplyInfo{
-				Delta:       big.NewInt(1999972496000000000),
-				Reward:      new(big.Int).Mul(common.Big2, big.NewInt(params.Ether)),
-				Withdrawals: common.Big0,
-				Burn:        big.NewInt(27504000000000),
-				Number:      2,
-				Hash:        common.HexToHash("0x2bb5afb410af25822a61b281393cf19d89663554422534d317de8f003b29e59c"), ParentHash: common.HexToHash("0x3053472d528da389264ca290e61d330fec1138ccb9d9ef686d5f1a8b68069b3b"),
-			},
-		},
-		{"Merge",
-			func(b *core.BlockGen) {},
-			live.SupplyInfo{
-				Delta:       common.Big0,
-				Reward:      common.Big0,
-				Withdrawals: common.Big0,
-				Burn:        common.Big0,
-				Number:      3,
-				Hash:        common.HexToHash("0x07d9204c8de4fc288e75ba8d90f4418314460492f8408d82896df2fd82d946e0"), ParentHash: common.HexToHash("0x2bb5afb410af25822a61b281393cf19d89663554422534d317de8f003b29e59c"),
-			},
-		},
-		{"Withdrawals",
-			func(b *core.BlockGen) {
-				b.AddWithdrawal(&types.Withdrawal{
-					Validator: 42,
-					Address:   common.Address{0xee},
-					Amount:    1337,
-				})
-			},
-			live.SupplyInfo{
-				Delta:       big.NewInt(1337000000000),
-				Reward:      common.Big0,
-				Withdrawals: big.NewInt(1337000000000),
-				Burn:        common.Big0,
-				Number:      4,
-				Hash:        common.HexToHash("0x9259ce2600b670f7369f699bb325cc57f646d0f392c98b29022596f54252db95"), ParentHash: common.HexToHash("0x07d9204c8de4fc288e75ba8d90f4418314460492f8408d82896df2fd82d946e0"),
-			},
-		},
+		b.AddTx(tx)
 	}
+
+	expected := live.SupplyInfo{
+		Delta:       big.NewInt(1999975934000000000),
+		Reward:      new(big.Int).Mul(common.Big2, big.NewInt(params.Ether)),
+		Withdrawals: common.Big0,
+		Burn:        big.NewInt(24066000000000),
+		Number:      1,
+		Hash:        common.HexToHash("0x9910ef69af46d01093bd74da6045c0a2d37adf158001d1df5abb4106d85f1aeb"), ParentHash: common.HexToHash("0x7469edd360a63bcf47b7ab6e1a28e5ace54985138d99f100191fef012d73cf32"),
+	}
+
+	out, err := testSupplyTracer(gspec, eip1559BlockGenerationFunc)
+	if err != nil {
+		t.Fatalf("failed to test supply tracer: %v", err)
+	}
+
+	actual := out[expected.Number]
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("incorrect supply info: expected %+v, got %+v", expected, actual)
+	}
+}
+
+func TestSupplyWithdrawals(t *testing.T) {
+	var (
+		merger = consensus.NewMerger(rawdb.NewMemoryDatabase())
+		config = *params.AllEthashProtocolChanges
+
+		gspec = &core.Genesis{
+			Config: &config,
+		}
+	)
+
+	shanghaiTime := uint64(0)
+	gspec.Config.ShanghaiTime = &shanghaiTime
+
+	// Activate merge since genesis
+	merger.ReachTTD()
+	merger.FinalizePoS()
+
+	// Set the terminal total difficulty in the config
+	gspec.Config.TerminalTotalDifficulty = big.NewInt(0)
+
+	withdrawalsBlockGenerationFunc := func(b *core.BlockGen) {
+		b.SetPoS()
+
+		b.AddWithdrawal(&types.Withdrawal{
+			Validator: 42,
+			Address:   common.Address{0xee},
+			Amount:    1337,
+		})
+	}
+
+	expected := live.SupplyInfo{
+		Delta:       big.NewInt(1337000000000),
+		Reward:      common.Big0,
+		Withdrawals: big.NewInt(1337000000000),
+		Burn:        common.Big0,
+		Number:      1,
+		Hash:        common.HexToHash("0xb85343cee6331f7dcdc1b1c8e698e0e63d47d84d8f81c9058fda830ac9368ec0"), ParentHash: common.HexToHash("0xc8265888895eb7715237be1c8730a1370f8fd8b33b6e4d5400413ebf33e4d3be"),
+	}
+
+	out, err := testSupplyTracer(gspec, withdrawalsBlockGenerationFunc)
+	if err != nil {
+		t.Fatalf("failed to test supply tracer: %v", err)
+	}
+
+	actual := out[expected.Number]
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("incorrect supply info: expected %+v, got %+v", expected, actual)
+	}
+}
+
+func testSupplyTracer(genesis *core.Genesis, gen func(*core.BlockGen)) ([]live.SupplyInfo, error) {
+	var (
+		engine = beacon.New(ethash.NewFaker())
+	)
 
 	// Load supply tracer
 	tracer, err := directory.LiveDirectory.New("supply")
 	if err != nil {
-		t.Fatalf("failed to create call tracer: %v", err)
+		return nil, fmt.Errorf("failed to create call tracer: %v", err)
 	}
 
-	chain, err := core.NewBlockChain(rawdb.NewMemoryDatabase(), core.DefaultCacheConfigWithScheme(rawdb.PathScheme), gspec, nil, engine, vm.Config{Tracer: tracer}, nil, nil)
+	chain, err := core.NewBlockChain(rawdb.NewMemoryDatabase(), core.DefaultCacheConfigWithScheme(rawdb.PathScheme), genesis, nil, engine, vm.Config{Tracer: tracer}, nil, nil)
 	if err != nil {
-		t.Fatalf("failed to create tester chain: %v", err)
+		return nil, fmt.Errorf("failed to create tester chain: %v", err)
 	}
 	defer chain.Stop()
 
-	genDb, blocks, _ := core.GenerateChainWithGenesis(gspec, engine, 1, func(i int, b *core.BlockGen) {
+	_, blocks, _ := core.GenerateChainWithGenesis(genesis, engine, 1, func(i int, b *core.BlockGen) {
 		b.SetCoinbase(common.Address{1})
-		tests[b.Number().Uint64()].input(b)
+		gen(b)
 	})
 
 	if n, err := chain.InsertChain(blocks); err != nil {
-		t.Fatalf("block %d: failed to insert into chain: %v", n, err)
-	}
-
-	// London hard fork
-	gspec.BaseFee = big.NewInt(params.InitialBaseFee)
-
-	parent := blocks[len(blocks)-1]
-	blocks, _ = core.GenerateChain(gspec.Config, parent, engine, genDb, 1, func(i int, b *core.BlockGen) {
-		b.SetCoinbase(common.Address{2})
-		tests[b.Number().Uint64()].input(b)
-	})
-
-	if n, err := chain.InsertChain(blocks); err != nil {
-		t.Fatalf("block %d: failed to insert into chain: %v", n, err)
-	}
-
-	// The Merge
-	merger.ReachTTD()
-	merger.FinalizePoS()
-	// Set the terminal total difficulty in the config
-	ttd := big.NewInt(int64(len(blocks)))
-	ttd.Mul(ttd, params.GenesisDifficulty)
-	gspec.Config.TerminalTotalDifficulty = ttd
-
-	parent = blocks[len(blocks)-1]
-	blocks, _ = core.GenerateChain(gspec.Config, parent, engine, genDb, 2, func(i int, b *core.BlockGen) {
-		b.SetPoS()
-		b.SetCoinbase(common.Address{3})
-		tests[b.Number().Uint64()].input(b)
-	})
-
-	if n, err := chain.InsertChain(blocks); err != nil {
-		t.Fatalf("block %d: failed to insert into chain: %v", n, err)
+		return nil, fmt.Errorf("block %d: failed to insert into chain: %v", n, err)
 	}
 
 	// Check and compare the results
 	// TODO: replace file to pass results
 	file, err := os.OpenFile("supply.txt", os.O_RDONLY, 0666)
 	if err != nil {
-		t.Fatalf("failed to open file: %v", err)
+		return nil, fmt.Errorf("failed to open output file: %v", err)
 	}
 	defer file.Close()
 
+	var output []live.SupplyInfo
 	scanner := bufio.NewScanner(file)
-
-	testedBlockNumber := 0
 
 	for scanner.Scan() {
 		blockBytes := scanner.Bytes()
 
-		var actual live.SupplyInfo
-		if err := json.Unmarshal(blockBytes, &actual); err != nil {
-			t.Fatalf("failed to unmarshal result for block %d: %v", testedBlockNumber, err)
+		var info live.SupplyInfo
+		if err := json.Unmarshal(blockBytes, &info); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal result: %v", err)
 		}
 
-		test := tests[testedBlockNumber]
-		expected := test.expected
-		if !reflect.DeepEqual(actual, expected) {
-			t.Fatalf("Test \"%v\" at block %d: incorrect supply info: expected %+v, got %+v", test.name, testedBlockNumber, expected, actual)
-		}
-
-		testedBlockNumber++
+		output = append(output, info)
 	}
+
+	return output, nil
 }

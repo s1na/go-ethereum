73d0f304d441e6f40e1489b7ea59417140165150
Sina Mahmoodi <itz.s1na@gmail.com>
2024-05-13 16:28:05 +0200

    miner: process deposit requests

--- miner/worker.go
+++ miner/worker.go
@@ -105,7 +105,24 @@ func (miner *Miner) generateWork(params *generateParams) *newPayloadResult {
 			log.Warn("Block building is interrupted", "allowance", common.PrettyDuration(miner.config.Recommit))
 		}
 	}
+
 	body := types.Body{Transactions: work.txs, Withdrawals: params.withdrawals}
+	allLogs := make([]*types.Log, 0)
+	for _, r := range work.receipts {
+		allLogs = append(allLogs, r.Logs...)
+	}
+	// Read requests if Prague is enabled.
+	if miner.chainConfig.IsPrague(work.header.Number, work.header.Time) {
+		requests, err := core.ParseDepositLogs(allLogs)
+		if err != nil {
+			return &newPayloadResult{err: err}
+		}
+		context := core.NewEVMBlockContext(work.header, miner.chain, nil)
+		vmenv := vm.NewEVM(context, vm.TxContext{}, work.state, miner.chainConfig, vm.Config{})
+		wxs := core.ProcessDequeueWithdrawalRequests(vmenv, work.state)
+		requests = append(requests, wxs...)
+		body.Requests = requests
+	}
 	block, err := miner.engine.FinalizeAndAssemble(miner.chain, work.header, work.state, &body, work.receipts)
 	if err != nil {
 		return &newPayloadResult{err: err}

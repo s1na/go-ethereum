e16e4b79a608193a9246eb70bfa81d2574c1e034
Sina Mahmoodi <itz.s1na@gmail.com>
2024-12-06 17:56:52 +0100

    rm 7742 calcBaseFee

--- consensus/misc/eip7742/eip7742.go
+++ consensus/misc/eip7742/eip7742.go
@@ -78,25 +78,3 @@ func CalcExcessBlobGas(parentExcessBlobGas, parentBlobGasUsed, targetBlobCount u
 	}
 	return excessBlobGas - (targetBlobCount * params.BlobTxBlobGasPerBlob)
 }
-
-// CalcBlobFee calculates the blobfee from the header's excess blob gas field.
-func CalcBlobFee(excessBlobGas uint64) *big.Int {
-	return fakeExponential(minBlobGasPrice, new(big.Int).SetUint64(excessBlobGas), blobGaspriceUpdateFraction)
-}
-
-// fakeExponential approximates factor * e ** (numerator / denominator) using
-// Taylor expansion.
-func fakeExponential(factor, numerator, denominator *big.Int) *big.Int {
-	var (
-		output = new(big.Int)
-		accum  = new(big.Int).Mul(factor, denominator)
-	)
-	for i := 1; accum.Sign() > 0; i++ {
-		output.Add(output, accum)
-
-		accum.Mul(accum, numerator)
-		accum.Div(accum, denominator)
-		accum.Div(accum, big.NewInt(int64(i)))
-	}
-	return output.Div(output, denominator)
-}
